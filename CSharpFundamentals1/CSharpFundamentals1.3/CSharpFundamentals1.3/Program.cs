9f712db8a3a78901141b4707e49471ecdee250c6
yani <157167562+359cm@users.noreply.github.com>
2025-05-08 19:04:47 +0300

    Add files via upload

---
+++ CSharpFundamentals1/CSharpFundamentals1.3/CSharpFundamentals1.3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* #1
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+int[] numbers = { num1, num2, num3 };
+Array.Sort(numbers);
+Array.Reverse(numbers);
+
+foreach (int num in numbers)
+{
+    Console.WriteLine(num);
+} */
+
+
